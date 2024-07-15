32b555b03907ecef13b95d65b6f0b6b40ccea00e
kamiya <nanahumi0928@gmail.com>
2024-07-15 15:53:28 +0900

    feat: import icon

--- app/components/profile/profile.tsx
+++ app/components/profile/profile.tsx
@@ -1,10 +1,19 @@
 import styles from "./profile.module.css";
+import Image from "next/image";
+import localImage from "@/public/icon.png";
 
 export const Profile = () => {
   return (
     <div className={styles.profile}>
-      <h1>Profile</h1>
-      <p>hogehoge</p>
+      <Image
+        className={styles.iconImage}
+        src={localImage}
+        width={150}
+        height={150}
+        alt="profile picture"
+      />
+      <h1 className="my-5">プロフィール</h1>
+      <p>ほげほげほげ</p>
     </div>
   );
 };
