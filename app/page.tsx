16632290ee86580c7474cada1df9085f2973acec
kamiya <nanahumi0928@gmail.com>
2024-07-15 17:25:33 +0900

    chore: modify profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import { FixedFooter } from "./components/footer/footer";
 import { Header } from "./components/header/header";
 import { Profile } from "./components/profile/profile";
+import Image from "next/image";
+import localImage from "@/public/icon.png";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Image
+        className="rounded-full mx-auto mt-8"
+        src={localImage}
+        width={150}
+        height={150}
+        alt="profile picture"
+      />
       <main>
         <Profile />
       </main>
